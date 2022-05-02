1e830ddc27d0c75a8bbe96e0efe0399b04a98f1b
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-05-02 22:03:37 +0200

    Created basic html structure

---
+++ src/components/AquariumHeader/AquariumHeader.js
@@ -0,0 +1,14 @@
+const AquariumHeader = () => {
+  return (
+    <ul>
+      <li>
+        <h1>Fishypedia</h1>
+      </li>
+      <li>
+        <h2>AQUARIUM</h2>
+      </li>
+    </ul>
+  );
+};
+
+export default AquariumHeader;
