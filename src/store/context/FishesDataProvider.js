1a95992537602342f29186c30bda511900525251
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-03 21:36:18 +0200

    Added initial state at useReducer of fishesReducer

--- src/store/context/FishesDataProvider.js
+++ src/store/context/FishesDataProvider.js
@@ -3,7 +3,7 @@ import fishesReducer from "../reducers/FishesReducer";
 import FishesDataContext from "./FishesDataContext";
 
 const FishesDataProvider = ({ children }) => {
-  const [fishes, dispatchFishes] = useReducer(fishesReducer);
+  const [fishes, dispatchFishes] = useReducer(fishesReducer, []);
 
   return (
     <FishesDataContext.Provider values={(fishes, dispatchFishes)}>
