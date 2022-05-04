f301b618029c3a96f0b64ea69d7d6fba0c988b13
Boris <boris@randebrock.com>
2022-05-04 10:14:39 +0200

    tested primary and secondary button

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -11,4 +11,35 @@ describe("Given the Button component", () => {
       expect(theButton).toBeInTheDocument();
     });
   });
+  describe("When invoked with the text 'text`", () => {
+    test("Then it should render the text 'text on the rendered Button", () => {
+      const expectedButtonText = "text";
+
+      render(<Button text={"text"} />);
+
+      const ButtonText = screen.getByText(expectedButtonText);
+
+      expect(ButtonText).toBeInTheDocument();
+    });
+  });
+  describe("When invoked with primary props", () => {
+    test("Then it should render a dark blue background", () => {
+      render(<Button styleType={"primary"} />);
+
+      const colorPrimaryButton = screen.getByRole("button");
+      expect(colorPrimaryButton).toHaveStyle(`
+ background: rgb(66, 174, 186);
+      color: rgb(250, 244, 237);
+    `);
+    });
+  });
+  test("Then it should render a light blue background", () => {
+    render(<Button styleType={"secondary"} />);
+
+    const colorPrimaryButton = screen.getByRole("button");
+    expect(colorPrimaryButton).toHaveStyle(`
+ background: rgba(145, 217, 217, 0.25);
+      color: rgb(66, 174, 186);
+    `);
+  });
 });
