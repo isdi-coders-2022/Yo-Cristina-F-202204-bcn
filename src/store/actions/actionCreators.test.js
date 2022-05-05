1e61a33f38932e86faadaf22366600cf5d701490
Nacho Losada <ignaciolosadaprato@gmail.com>
2022-05-06 00:51:13 +0200

    Fixed actionCreators broken tests

--- src/store/actions/actionCreators.test.js
+++ src/store/actions/actionCreators.test.js
@@ -84,7 +84,7 @@ describe("Given the addLocalFishesActionCreator function", () => {
 describe("Given the deleteLocalFishesActionCreator function", () => {
   describe("When invoked with a group of 2 fishes", () => {
     test("Then it should return an object with the type of property 'delete-local-fishes'", () => {
-      const fishes = [
+      const localFishes = [
         {
           id: 3,
           "file-name": "boris_fish",
@@ -96,9 +96,9 @@ describe("Given the deleteLocalFishesActionCreator function", () => {
       ];
       const expectedLocalResult = {
         type: "delete-local-fishes",
-        fishes,
+        localFishes,
       };
-      const localActionResult = deleteLocalFishesActionCreator(fishes);
+      const localActionResult = deleteLocalFishesActionCreator(localFishes);
 
       expect(localActionResult).toEqual(expectedLocalResult);
     });
@@ -108,7 +108,7 @@ describe("Given the deleteLocalFishesActionCreator function", () => {
 describe("Given the modifyFishesActionCreator function", () => {
   describe("When invoked with a group of 3 fishes", () => {
     test("Then it should return an object with the type of property 'modify-local-fishes'", () => {
-      const fishes = [
+      const localFishes = [
         {
           id: 3,
           "file-name": "boris_fish",
@@ -124,9 +124,9 @@ describe("Given the modifyFishesActionCreator function", () => {
       ];
       const expectedLocalResult = {
         type: "modify-local-fishes",
-        fishes,
+        localFishes,
       };
-      const localActionResult = modifyLocalFishesActionCreator(fishes);
+      const localActionResult = modifyLocalFishesActionCreator(localFishes);
 
       expect(localActionResult).toEqual(expectedLocalResult);
     });
