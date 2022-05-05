cf99a400e363b679080dc6f9d2186f03b458d6e6
MariamLliso <contacto.mariamlliso@gmail.com>
2022-05-05 18:56:39 +0200

    Corrected disposition of header, footer and cards

--- src/App.js
+++ src/App.js
@@ -3,18 +3,43 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import FishAquariumListPage from "./pages/FishAquariumListPage/FishAquariumListPage";
 import FishTankListPage from "./pages/FishTankListPage/FishTankListPage";
+import styled from "styled-components";
+import theme from "./theme/theme";
+import backgroundImage from "./images/mobile-background.png";
+
+const ContainerStyle = styled.div`
+  background: linear-gradient(
+      180deg,
+      rgba(${theme.bluelightRgb}, 0.95) 0%,
+      rgba(${theme.blueDarkRgb}, 0.95) 100%
+    ),
+    url(${backgroundImage});
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .header {
+    position: fixed;
+    width: 100%;
+  }
+`;
 
 function App() {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<FishAquariumListPage />} />
-        <Route path="aquarium" element={<FishAquariumListPage />} />
-        <Route path="fishtank" element={<FishTankListPage />} />
-      </Routes>
+    <ContainerStyle>
+      <header className="header">
+        <Header />
+      </header>
+      <main className="main">
+        <Routes>
+          <Route path="/" element={<FishAquariumListPage />} />
+          <Route path="aquarium" element={<FishAquariumListPage />} />
+          <Route path="fishtank" element={<FishTankListPage />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </ContainerStyle>
   );
 }
 
